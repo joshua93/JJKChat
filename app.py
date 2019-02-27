42abeee3a66c7c68a592266b9dc3ecbac689f094
Kenneth Padro <kenneth.padro@upr.edu>
2019-02-27 16:06:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from handler.user import UserHandler
 from handler.group import GroupHandler
 from handler.post import PostHandler
 from handler.hashtag import HashtagHandler
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def home():

186ec5c1689e175ee7f2db1eda1caaeee1d58974
Jesiniel Nieves <jesi.nieves@gmail.com>
2019-03-30 17:43:55 -0400

    Update group.py
    Arregle el orden del query para que funcione con el map to dictionary

--- dao/group.py
+++ dao/group.py
@@ -37,7 +37,7 @@ class GroupDAO:
     ##Changed by Jesi
     def getMembersByGroupID(self,gID):
         cursor = self.conn.cursor()
-        query = "select username, first_name, last_name, phone, email from chat_group_members natural inner join users where chat_group_id = %s"
+        query = "select user_id, first_name, last_name, email, phone, username from chat_group_members natural inner join users where chat_group_id = %s"
         cursor.execute(query, (gID,))
         result = []
         for row in cursor:

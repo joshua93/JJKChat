2998cc8e6cc38cdc2dc673537e49775f755ce0d0
Kenneth Padro <kenneth.padro@upr.edu>
2019-04-03 12:36:22 -0400

    Delete dbconfig.py

--- config/dbconfig.py
+++
@@ -1,6 +0,0 @@
-pg_config = {
-    'user' : 'cjyslzsdlngjec',
-    'passwd' : '38ee7f76f1b1345d5096d3f913b87155742a7d39c6c507ae4359a6b4a03c99ff',
-    'dbname' : 'd95sj7ufkghhjj',
-    'host' : 'ec2-50-19-208-11.compute-1.amazonaws.com'
-}

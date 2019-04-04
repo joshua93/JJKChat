4bafb404df86f7e7149dfc0acca97ec977b2229b
Joshua A Bonilla <joshua.bonilla@upr.edu>
2019-04-03 22:57:11 -0400

    Statistics Hashtags
    Trending topics fully implemented and working

--- dao/hashtag.py
+++ dao/hashtag.py
@@ -1,7 +1,14 @@
 from dao.data import Data
+import psycopg2
+from config.dbconfig import pg_config
 
 
 class HashtagDAO:
+
+    def __init__(self):
+        #DATABASE_URL = 'postgres://postgres:databaseclass@localhost:5432/jjkchat'
+        DATABASE_URL = "dbname=%s user=%s password=%s host=%s" % (pg_config['dbname'], pg_config['user'], pg_config['passwd'], pg_config['host'])
+        self.conn = psycopg2._connect(DATABASE_URL)
 
     hashtags = Data().hashtags
 
@@ -17,5 +24,10 @@ class HashtagDAO:
         return hashtag
 
     def getTrendingHashtag(self):
-        hashtag = list(filter(lambda u: u['text'] == '#MINI', self.hashtags))
-        return hashtag
+        cursor = self.conn.cursor()
+        query = "SELECT hashtag, ROW_NUMBER ( )  OVER(ORDER BY count(*) DESC) AS position FROM hashtags GROUP BY hashtag"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
